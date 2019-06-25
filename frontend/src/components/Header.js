67783486a23d32d848e2082a02c2325afde51162
vini-coelho <vinicius_coelho@live.com>
2019-06-25 16:31:37 -0400

    stylizing frontend app

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.css';
+
+import logo from '../assets/logo.svg';
+import camera from '../assets/camera.svg';
 
 export default () =>
     
     <header id="main-header">
-        <div className="ehader-content">
-            <img src="" alt="Instashare"/>
-            <img src="" alt="Enviar publicação"/>
+        <div className="header-content">
+            <Link to='/'>
+                <img src={logo} alt="Instashare"/>
+            </Link>
+            <Link to='/new'>
+                <img src={camera} alt="Enviar publicação"/>
+            </Link>
         </div>
     </header>
