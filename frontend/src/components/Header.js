e12940536a9d12a608b69b6930c315d23b16e727
vini-coelho <vinicius_coelho@live.com>
2019-06-25 13:29:30 -0400

    Starting React frontend dev

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Header.css';
+
+export default () =>
+    
+    <header id="main-header">
+        <div className="ehader-content">
+            <img src="" alt="Instashare"/>
+            <img src="" alt="Enviar publicação"/>
+        </div>
+    </header>
---
+++ frontend/src/routes.js
@@ -0,0 +1,10 @@
+import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+export default () => 
+    <Switch>
+        <Route path='/' exact component={Feed}/>
+        <Route path='/new' component={New}/>
+    </Switch>
